e3ac27d64d003aecac96faea917b2c0d0e570593
Gabriel Arancibia <arancibiagabriel@outlook.com>
2021-04-06 19:13:58 -0600

    about updated

--- front-end/src/router/index.js
+++ front-end/src/router/index.js
@@ -3,6 +3,7 @@ import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
 import Container from '../views/Container.vue'
 import SignUp from '../views/SignUp.vue'
+import About from '../views/About.vue'
 
 Vue.use(VueRouter)
 
@@ -24,11 +25,7 @@ const routes = [{
 {
     path: '/about',
     name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-        import( /* webpackChunkName: "about" */ '../views/About.vue')
+    component: About
 }
 ]
 
